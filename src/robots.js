d4bd2e353ddb8383d26cc16429f4fe8437ab5360
ilva <122323092+09012022@users.noreply.github.com>
2023-03-13 22:26:44 +0100

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,57 @@
+export const robots = [
+ 
+  {
+    id: 2,
+    name: 'Bryan Adams',
+    username: 'Antonette',
+    email: 'Bryan@Adams.tv'
+  },
+  {
+    id: 3,
+    name: 'Frank Sinatra',
+    username: 'samy',
+    email: 'Frank@Sinatra.net'
+  },
+  {
+    id: 4,
+    name: 'Amy Winehouse',
+    username: 'Karianne',
+    email: 'Amy@Winehouse.org'
+  },
+  {
+    id: 5,
+    name: 'Michael Jackson',
+    username: 'Kamren',
+    email: 'Michael@Jackon.ca'
+  },
+  {
+    id: 6,
+    name: 'Elton John',
+    username: 'Leopoldo_Corkery',
+    email: 'Elton@John'
+  },
+  {
+    id: 7,
+    name: 'Prince',
+    username: 'Elwyn.Skiles',
+    email: 'Just@Princ.biz'
+  },
+  {
+    id: 8,
+    name: 'Tina Turner',
+    username: 'Maxime_Nienow',
+    email: 'Tina@Turner.me'
+  },
+  {
+    id: 9,
+    name: 'Lionel Richie',
+    username: 'Delphine',
+    email: 'Lionel@Richie'
+  },
+  {
+    id: 10,
+    name: 'Stevie Wonder',
+    username: 'Moriah.Stanton',
+    email: 'Stevie@Wonder'
+  }
+];
